be01e3d1b2d0098dfd29b0582faadc65c17c8768
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-05-25 17:14:00 -0400

    add service, toggle, get, put for todo items

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { TodoService } from "../../services/todo.service";
+
 import { Todo } from "src/app/models/Todo";
 
 @Component({
@@ -9,7 +11,7 @@ import { Todo } from "src/app/models/Todo";
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {}
 
@@ -20,5 +22,18 @@ export class TodoItemComponent implements OnInit {
       "is-complete": this.todo.completed
     };
     return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle in UI
+    todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    });
+  }
+
+  onDelete(todo) {
+    console.log("delete");
   }
 }
